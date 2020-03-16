34480a97c63b92c8c3f90772f61502ed1d65ec04
Lukas Gruszka <gallex.lukaszgruszka@gmail.com>
2020-03-16 16:47:05 +0100

    nowaStrona

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php include 'headeR.php'; ?>
+
+
+
+<div id="content">
+
+
+	
+	<p>Strona powstała w celu preznetacji kilku moich umiejętności związanych ze zdobyta wiedzą podczas ostatnich lat studiów. Po części pracę tą rozpocząłem na starzach w firmach zajmujacych się tworzeniem witryn pod różnego rodzaju aplikacjach. Sam jednak skupiłem się na systemach Linuxowych , opensouce serwerach zarządzanych autoamtycznie w kolejkach zadaniowych z wykorzystaniem php do prezentacji wszelkich wyników obliczeniowych. </p>
+
+	<div class="clear"></div>
+</div>
+
+
+
+
+<?php include 'footeR.php'; ?>
+
